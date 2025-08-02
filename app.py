3387fec997e1b50a3273048d416b2c23f54da5a9
Phantom-VK <vikramadityakhupse@gmail.com>
2025-08-02 13:40:56 +0530

    Integrated Streamlit

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from rnn.etl.etl import ETL
-from rnn.model import IMDBRNNMODEL
-import tensorflow as tf
+from keras.preprocessing import sequence
+import streamlit as st
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.models import load_model
 
-if __name__ == "__main__":
-    # model = IMDBRNNMODEL(optimizer='adam',loss='binary_crossentropy')
-    # X_train, y_train, X_test, y_test = ETL().getr_train_test_split()
-    # trained_model = model.train_model(X_train,y_train)
-    trained_model = tf.keras.models.load_model('trained_model/simple_rnn_imdb.keras')
-    print(trained_model.summary())
+word_index = imdb.get_word_index()
+reversed_word_index = {value: key for key, value in word_index.items()}
+
+model = load_model('trained_model/simple_rnn_imdb.keras')
+
+
+def decode_review(encoded_review):
+    return " ".join([reversed_word_index.get(i - 3, '?') for i in encoded_review])
+
+
+def preprocess_text(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word, 2) + 3 for word in words]
+    padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
+    return padded_review
+
+
+## App ui
+st.title('IMDB Movie Review Sentiment Analysis')
+st.write('Enter a movie review to classify it as positive or negative.')
+
+user_input = st.text_area('Movie Review')
+
+if user_input and st.button('Classify'):
+    preprocessed_input = preprocess_text(user_input)
+
+    ## MAke prediction
+    prediction = model.predict(preprocessed_input)
+    sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    # Display the result
+    st.write(f'Sentiment: {sentiment}')
+    st.write(f'Prediction Score: {prediction[0][0]}')
+else:
+    st.write('Please enter a movie review.')
